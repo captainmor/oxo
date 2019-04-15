2f522384ed415e6487cab584b6c90b3b0c9fe268
Moruling B. James Rubinsin <Moruling.JamesRubinsin@Haworth.com>
2019-04-15 11:32:18 +0800

    initial

---
+++ home.js
@@ -0,0 +1,13 @@
+function functionOne() {
+  alert('Piu piu piuuu');
+}
+
+function functionTwo() {
+  alert('You clicked the bottom text');
+}
+
+$(document).ready(function () {
+  $("#btnTest").click(function () {
+    alert('JQUERY TEST');
+  });
+});
